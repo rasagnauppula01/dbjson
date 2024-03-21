ac2e7acd277936d8d05b256a02b29e825b40721d
rasagnauppula01 <rasagnauppula10@gmail.com>
2024-03-21 14:13:21 +0530

    final

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -8,15 +7,4 @@ server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
-=======
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
-server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server is running')
->>>>>>> 460e78d69bb6cbedd87fa8db349da3a4ea3b80af
 })
